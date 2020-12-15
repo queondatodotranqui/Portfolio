360c95adec49f9d0298b96ab43a56a3d2d68bb6f
Nicolas Vera <57852440+queondatodotranqui@users.noreply.github.com>
2020-12-15 12:22:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 
-window.addEventListener('load', ()=>{
-    
+var links = document.querySelectorAll('a');
+
+links.forEach((link)=>{
+    link.addEventListener('click', (e)=>{
+
+        e.preventDefault();
+
+        history.pushState(null, '', e.target.innerHTML);
+    })
 })
